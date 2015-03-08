39c56bf8049a1b24f9a8e7ad6eb0ad4616b09fd6
Shayol <Vorga88@gmail.com>
2015-03-08 22:50:12 +0300

    add validations and scope to project and task models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ActiveRecord::Base
+
+  has_many :tasks, dependent: :destroy
+  validates :title, presence: true
+  default_scope -> { order('created_at DESC') } #projects retrieved in descending order from newest to oldest
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+  validates :content, presense: true
+  validates :priority, numericality: {:greater_than: 0}
+
+  default_scope -> { order('priority DESC') } #tasks retrieved in descending order from task with highest to lowest(1..lowest) priority
 end
