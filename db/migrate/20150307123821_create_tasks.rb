5f96c8a47a3e2977fa9db0636f129d82eb5f27e8
Shayol <Vorga88@gmail.com>
2015-03-17 16:09:32 +0300

    unlawfully change migration to add zero as default priority in task model

--- db/migrate/20150307123821_create_tasks.rb
+++ db/migrate/20150307123821_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.text :content
-      t.integer :priority
+      t.integer :priority, null: false, default: 0
       t.references :project, index: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20150314130438) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
-    t.integer  "priority"
+    t.integer  "priority",   default: 0,     null: false
     t.integer  "project_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
