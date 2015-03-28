bbe5eebda7e64fd37667f3221b495109b66de409
Shayol <Vorga88@gmail.com>
2015-03-28 11:56:36 +0300

    add foreign key for user has many projects association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
 
+  belongs_to :user
   has_many :tasks, dependent: :destroy
   validates :title, presence: true
   default_scope -> { order('created_at DESC') } #projects retrieved in descending order from newest to oldest
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328073701) do
+ActiveRecord::Schema.define(version: 20150328084702) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
